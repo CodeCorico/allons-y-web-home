581421ccbe1723f42bb2ec7b289eef911327b20c
Xavier Boubert <xavier@boubert.fr>
2016-09-30 13:34:27 +0200

    feat(web-home): use the new allons-y-web contexts autoOpen system

--- features/web-home/views/web-home-index.js
+++ features/web-home/views/web-home-index.js
@@ -3,6 +3,8 @@
 
   window.bootstrap(['$Page', '$i18nService', '$done', function($Page, $i18nService, $done) {
 
+    $Page.remember(/^\/?$/);
+
     $Page.set('apps', [{
       name: $i18nService._('Home'),
       selected: true,
